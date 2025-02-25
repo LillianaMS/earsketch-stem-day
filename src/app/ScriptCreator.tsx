f77de25394cfd8b621d95f21826781dbd7e3a68a
Lilliana Marrero <marrerosolis@gmail.com>
2025-02-25 12:40:34 -0400

    Query secondary db to store newly created song for mom.

--- src/app/ScriptCreator.tsx
+++ src/app/ScriptCreator.tsx
@@ -1,81 +1,124 @@
 import React, { useState } from "react"
 import { useTranslation } from "react-i18next"
 import { useSelector } from "react-redux"
+import axios from "axios";
 
 import * as app from "../app/appState"
 import * as scriptsState from "../browser/scriptsState"
 import store from "../reducers"
 import { ModalFooter, ModalHeader, ModalBody, Alert } from "../Utils"
+import { set } from "lodash";
 
-export function validateScriptName(name: string, extension: string) {
-    const fullname = name + extension
+export function validateScriptName(extension: string, qrCodeNum: string, firstName: string) { // Añadir validación para los nuevos datos
+    const scriptName = qrCodeNum + "_" + firstName.toLowerCase() + extension
     const scripts = scriptsState.selectRegularScripts(store.getState())
 
-    if (name.length < 3) {
+    if (scriptName.length < 3) {
         throw new Error("messages:general.shortname")
-    } else if (/[$-/:-?{-~!"^#`[\]\\]/g.test(name)) {
+    } else if (/[$-/:-?{-~!"^#`[\]\\]/g.test(qrCodeNum + name)) {
         // Why are hyphens banned from script names?
         throw new Error("messages:idecontroller.illegalname")
-    } else if (Object.values(scripts).some(script => !script.soft_delete && script.name.toLocaleUpperCase() === fullname.toLocaleUpperCase())) {
+    } else if (Object.values(scripts).some(script => !script.soft_delete && script.name.toLocaleUpperCase() === scriptName.toLocaleUpperCase())) {
         // Conflict with existing non-deleted script.
         throw new Error("messages:idecontroller.overwrite")
-    } else if (Object.values(scripts).some(script => script.soft_delete && script.name.toLocaleUpperCase() === fullname.toLocaleUpperCase())) {
+    } else if (Object.values(scripts).some(script => script.soft_delete && script.name.toLocaleUpperCase() === scriptName.toLocaleUpperCase())) {
         // Conflict with existing deleted script.
         throw new Error("messages:idecontroller.overwriteDeleted")
     } else if (![".py", ".js"].includes(extension)) {
         throw new Error("messages:idecontroller.illegalname")
     } else {
         // Valid name.
-        return name + extension
+        return scriptName
     }
 }
 
 export const ScriptCreator = ({ close }: { close: (value?: any) => void }) => {
     const language = useSelector(app.selectScriptLanguage)
-    const [name, setName] = useState("")
     const [error, setError] = useState("")
     const [extension, setExtension] = useState(language === "python" ? ".py" : ".js")
     const { t } = useTranslation()
 
+    const [qrCodeNum, setQrCodeNum] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    // const [scriptName, setScriptName] = useState("")
+    // const [mp3Url, setMp3Url] = useState("")
+    const [shareID, setShareID] = useState("")
+    const [shareUrl, setShareUrl] = useState("")
+    
     const confirm = () => {
         try {
-            close(validateScriptName(name, extension))
+            close(validateScriptName(extension, qrCodeNum, firstName)) // Añadir validación para los nuevos datos
         } catch (error) {
             setError(error.message)
         }
     }
 
+    async function registerSong(qrCodeNum: string, firstName: string, lastName: string, email: string ): Promise<void> {
+        const prServerDir = "remoodle.fun/nsf-stem-day/canciones/"
+        const mp3Url = prServerDir + qrCodeNum + ".mp3"
+        const scriptName = qrCodeNum + "_" + firstName.toLowerCase() + extension
+        // No necesito (y no tengo) el shareID y shareUrl. Esos serán un update al DB luego de que el script se haya creado
+        try {
+            const response = await axios.post("http://localhost:8081/api/registry", { "qrCodeNum": qrCodeNum, "firstName": firstName, "lastName": lastName, "email": email, "scriptName": scriptName, "mp3Url": mp3Url })
+            console.log(JSON.stringify(response.data))
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
     return <>
         <ModalHeader>{t("scriptCreator.title")}</ModalHeader>
-        <form onSubmit={e => { e.preventDefault(); confirm() }}>
+        <form onSubmit={e => { e.preventDefault(); registerSong(qrCodeNum, firstName, lastName, email); confirm() }}>
             <ModalBody>
                 <Alert message={t(error)}></Alert>
                 <div className="flex mb-2">
                     <div className="form-group w-1/2 mx-6">
-                        <label>{t("scriptCreator.scriptName")}</label>
+                        <label>{t("scriptCreator.qrCodeNum")}</label>
                         <p className="text-sm">{t("scriptCreator.scriptName.subtext")}</p>
                     </div>
                     <div className="form-group w-1/2 mx-6">
-                        <label>{t("scriptCreator.scriptLang")}</label>
-                        <p className="text-sm">{t("scriptCreator.scriptLang.subtext")}</p>
+                        <label>{t("scriptCreator.firstName")}</label>
+                        <p className="text-sm">{t("scriptCreator.scriptName.subtext")}</p>
+                    </div>
+                    <div className="form-group w-1/2 mx-6">
+                        <label>{t("scriptCreator.lastName")}</label>
+                        <p className="text-sm">{t("scriptCreator.scriptName.subtext")}</p>
+                    </div>
+                    <div className="form-group w-1/2 mx-6">
+                        <label>{t("scriptCreator.email")}</label>
+                        <p className="text-sm">{t("scriptCreator.scriptName.subtext")}</p>
                     </div>
                 </div>
                 <div className="flex">
                     <div className="w-1/2 mx-6 relative">
                         <input className="form-input w-full dark:bg-transparent placeholder:text-gray-300" autoFocus autoComplete="off"
-                            name={t("scriptCreator.scriptName")} id="scriptName" placeholder={t("scriptCreator.scriptName")}
-                            title={t("scriptCreator.scriptName")} aria-label={t("scriptCreator.scriptName")}
-                            value={name} onChange={e => setName(e.target.value)} />
-                        <div className="absolute inset-y-0 right-0 flex items-center mr-2 text-gray-500 dark:text-gray-300">{extension}</div>
+                            name={t("scriptCreator.qrCodeNum")} id="qrCodeNum" placeholder={t("scriptCreator.qrCodeNum")}
+                            title={t("scriptCreator.qrCodeNum")} aria-label={t("scriptCreator.qrCodeNum")}
+                            value={qrCodeNum} onChange={e => setQrCodeNum(e.target.value)} />
                     </div>
-
-                    <select className="form-select w-1/2 mx-6 dark:bg-transparent placeholder:text-gray-300" value={extension} onChange={e => setExtension(e.target.value)} title={t("curriculum.switchScriptLanguage")} aria-label={t("curriculum.switchScriptLanguage")}>
-                        <option value=".py">Python</option>
-                        <option value=".js">JavaScript</option>
-                    </select>
+                    <div className="w-1/2 mx-6 relative">
+                        <input className="form-input w-full dark:bg-transparent placeholder:text-gray-300" autoFocus autoComplete="off"
+                            name={t("scriptCreator.firstName")} id="firstName" placeholder={t("scriptCreator.firstName")}
+                            title={t("scriptCreator.firstName")} aria-label={t("scriptCreator.firstName")}
+                            value={firstName} onChange={e => setFirstName(e.target.value)} />
+                    </div>
+                    <div className="w-1/2 mx-6 relative">
+                        <input className="form-input w-full dark:bg-transparent placeholder:text-gray-300" autoFocus autoComplete="off"
+                            name={t("scriptCreator.lastName")} id="lastName" placeholder={t("scriptCreator.lastName")}
+                            title={t("scriptCreator.lastName")} aria-label={t("scriptCreator.lastName")}
+                            value={lastName} onChange={e => setLastName(e.target.value)} />
+                    </div>
+                    <div className="w-1/2 mx-6 relative">
+                        <input className="form-input w-full dark:bg-transparent placeholder:text-gray-300" autoFocus autoComplete="off"
+                            name={t("scriptCreator.email")} id="email" placeholder={t("scriptCreator.email")}
+                            title={t("scriptCreator.email")} aria-label={t("scriptCreator.email")}
+                            value={email} onChange={e => setEmail(e.target.value)} />
+                    </div>
                 </div>
             </ModalBody>
-            <ModalFooter submit="create" close={close} />
+            <ModalFooter submit="create" close={close} /> {/* añadir aquí una función para enviar datos a nuestra DB? Tendría que modificar la definición del ModalFooter */}
         </form>
     </>
 }
